09335f0aecf6d299a58a88ddb2caf6986e507559
Kelvin George <kelvingeorge.anime23@gamil.com>
2026-07-01 22:24:09 +0300

    Added image path

--- plotwatch/settings.py
+++ plotwatch/settings.py
@@ -91,7 +91,7 @@ STORAGES = {
 }
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.getenv('MEDIA_ROOT', str(BASE_DIR / 'media'))
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- plotwatch/urls.py
+++ plotwatch/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('admin/overview/', core_views.admin_overview, name='admin_overview'),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
